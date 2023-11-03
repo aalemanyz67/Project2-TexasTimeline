18f5c2d2dc89fd82f7600936774def824696e5a6
elmfer <elmfer10@gmail.com>
2023-11-03 14:21:08 -0500

    Added user model

---
+++ develop/models/User.js
@@ -0,0 +1,36 @@
+const sequelize = require('../config/connection');
+
+class User extends Model {};
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
