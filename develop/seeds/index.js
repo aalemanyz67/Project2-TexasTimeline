35b9c3cee8f6a1852df486149b39c93dc42dd628
elmfer <elmfer11@outlook.com>
2023-11-06 11:14:45 -0600

    Use inquirer v8

--- develop/seeds/index.js
+++ develop/seeds/index.js
@@ -1,22 +1,25 @@
 const sequelize = require('../config/connection');
 const inquirer = require('inquirer');
-const loading = require('./loading');
+const loading = require('loading-cli');
 const { User, Event, Comment } = require('../models');
 
 function welcomeUser() {
-  console.log('+--------------------------------+');
-  console.log('|                                |');
-  console.log('|   Welcome to the Event Seeder  |');
-  console.log('|                                |');
-  console.log('+--------------------------------+\n\n');
+  console.log('+------------------------------------+');
+  console.log('|                                    |');
+  console.log('|     Welcome to the Event Seeder    |');
+  console.log('|                                    |');
+  console.log('+------------------------------------+\n\n');
 }
 
 async function syncDatabase() {
-
+  const load = loading("Syncing to database").start();
+  await sequelize.sync({ force: true });
+  load.stop();
 }
 
 async function seedDatabase() {
   welcomeUser();
+  await syncDatabase();
 }
 
 seedDatabase();
