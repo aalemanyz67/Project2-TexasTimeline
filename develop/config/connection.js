eccfcc0e6679805afa233c0c366f41102f176eb7
elmfer <elmfer11@outlook.com>
2023-11-06 11:15:39 -0600

    sql connection to use DB_HOST enviornment variable

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
   }
