62c40c9e3bedb1ff85fef91fc556be2f1edfd76d
Billy Finch <billyrfinch@gmail.com>
2023-11-06 23:28:31 -0600

    api is working

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -1,19 +1,13 @@
-const Sequelize = require('sequelize');
-
-// Enable access to .env variables
 require('dotenv').config();
-
-// Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+const Sequelize = require('sequelize');
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306,
-    logging: false
-  }
-);
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
